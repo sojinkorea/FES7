39e76076c05e6ff452f4bc98f8fc797b829b0e01
sojinjang <wkdthwls16@naver.com>
2023-09-11 17:44:29 +0900

    📝 2023.09.11

---
+++ React/20230911/my-app/src/App3.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Login from "./Components/Login";
+import Homepage from "./Components/Homepage";
+
+export default function App3() {
+  const user = {
+    idUser: "thwls@naver.com",
+    pwUser: 1234,
+  };
+
+  // 로그인 상태를 판단하는 state
+  const [login, setLogin] = useState(false);
+
+  return login ? <Homepage /> : <Login infoUser={user} setLogin={setLogin} />;
+}
