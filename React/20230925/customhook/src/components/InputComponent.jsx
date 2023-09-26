e6561492cf654c1df4f8f11fab7261eaacac7ba1
sojinjang <wkdthwls16@naver.com>
2023-09-26 15:44:25 +0900

    📝 2023.09.25 수정

--- React/20230925/customhook/src/components/InputComponent.jsx
+++ React/20230925/customhook/src/components/InputComponent.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
+import useInput from "../Hooks/useInput";
 
 function InputComponent() {
-  const [value, setValue] = useState("");
-  function onChange(e) {
-    setValue(e.target.value);
-  }
+  const [value, onChange] = useInput("");
+
   return (
     <>
       <input type="text" onChange={onChange} />
